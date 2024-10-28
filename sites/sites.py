514874988edb2ea75e6c486552afa075449a1990
LYHOFOX <104648720+LYHOFOX@users.noreply.github.com>
2024-10-28 22:03:23 +0800

    Update sites.py

--- sites/sites.py
+++ sites/sites.py
@@ -5,6 +5,7 @@ user_agent = 'CharlesBot/0.0.1 (Charles@klei.vip)'
 wikigg = Site(host='noita.wiki.gg',
               path="/zh/", clients_useragent=user_agent)
 
-bwiki = Site(host='wiki.biligame.com', path="/noita/", clients_useragent=user_agent)
+bwiki = Site(host='wiki.biligame.com', 
+             path="/noita/", clients_useragent=user_agent)
 
 # fandom = Site(host="oxygennotincluded.fandom.com", path="/zh/", clients_useragent=user_agent)
