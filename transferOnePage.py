f869a510ebe1c3d7c244880779778cd519bc846b
LYHOFOX <104648720+LYHOFOX@users.noreply.github.com>
2024-10-31 16:37:08 +0800

    Update transferOnePage.py

--- transferOnePage.py
+++ transferOnePage.py
@@ -3,15 +3,6 @@ import os
 from pages.transferOnePage import transferPage
 from sites.sites import bwiki, wikigg
 
-wikigg_user_name = ''
-wikigg_user_password = ''
-bwiki_session_data = ''
-
-if 'GITHUB_ACTIONS' in os.environ:
-    wikigg_user_name = os.environ.get('WIKIGG_USER')
-    wikigg_user_password = os.environ.get('WIKIGG_USER_PASSWORD')
-    bwiki_session_data = os.environ.get("BWIKI_SESSION_DATA")
-
 def transferPage(oldSite: Site, newSite: Site, pageName: str, username: str, password: str, sessiondata: str):
     # 登录到旧站点
     oldSite.login(username, password)
